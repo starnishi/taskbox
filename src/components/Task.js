78a1af637dc9cd5e96070f6e9846744885dcf29d
star_nishi <nishiyama@jurabi.jp>
2021-01-23 13:35:08 +0900

    Intro to Storybook finished

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,7 @@
 // src/components/Task.js
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
@@ -29,3 +30,20 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     </div>
   );
 }
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
+
